927ff10d4e027d8a96232ecf9906bde7a2c74f70
Ekaterina Krivich <ekateinak@heathmont.net>
2022-09-16 16:52:49 +0300

    downloading page

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
     "io/ioutil"
     "log"
     "github.com/gin-gonic/gin"
+    "strconv"
+    "time"
 )
 
 type oneLecture struct {
@@ -14,22 +16,40 @@ type oneLecture struct {
     Code string `json:"code"`
 }
 
+var uriPartEng = "keel=1"
+var link = "http://www.tlu.ee/masio/?id=aine&aine="
+var nextWeekSec = int64(800608)
 
-// http://www.tlu.ee/masio/?id=aine&aine=IFI8110.DT&time=1663794000&keel=1
-func getLecturesByCode(code string)([]oneLecture) {
+func uriPartCurrentTime() (string) {
+    t := time.Now()
+    sec := t.Unix() + nextWeekSec
+    return "time=" + strconv.Itoa(int(sec))
+}
+
+func parseLectureInfo(pageContent string)(oneLecture) {
+    lecture := oneLecture{Date: "some date", Time: "some time", Room: "some room", Code: "some code"}
+    return lecture 
+}
+
+func getLectureByCode(code string)([]oneLecture) {
+    url := link + code + "&" + uriPartCurrentTime() + "&" + uriPartEng
+    log.Print("Requesting: " + url)
+    pageContent := getPageContent(url)
+    lecture := parseLectureInfo(pageContent)
+    log.Print(pageContent)
     return []oneLecture{
-       {Date: "Wednesday 22. September", Time: "10:00-13:00", Room: "A-343", Code: code},
+       lecture,
     }
 }
 
 // getLectures responds with the list of all lectures this week as JSON.
 func getLectures(c *gin.Context) {
     code := c.Param("code")
-    lectures := getLecturesByCode(code)
+    lectures := getLectureByCode(code)
     c.IndentedJSON(http.StatusOK, lectures)
 }
 
-func pageContent(link string)(string) {
+func getPageContent(link string)(string) {
     res, err := http.Get(link)
     if err != nil {
         log.Fatal(err)
