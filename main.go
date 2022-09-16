6bee1283804c471cc62d47141c2b576e6d60c49a
Ekaterina Krivich <ekateinak@heathmont.net>
2022-09-16 15:53:39 +0300

    dynamic url

--- main.go
+++ main.go
@@ -2,30 +2,34 @@ package main
 
 import (
     "net/http"
-    "fmt"
     "io/ioutil"
     "log"
     "github.com/gin-gonic/gin"
 )
 
 type oneLecture struct {
-	Date string `json:"date"`
-	Time string `json:"time"`
-	Room string `json:"room"`
+    Date string `json:"date"`
+    Time string `json:"time"`
+    Room string `json:"room"`
+    Code string `json:"code"`
 }
 
-var lectures = []oneLecture{
-	{Date: "Wednesday 22. September", Time: "10:00-13:00", Room: "A-343"},
-}
 
 // http://www.tlu.ee/masio/?id=aine&aine=IFI8110.DT&time=1663794000&keel=1
+func getLecturesByCode(code string)([]oneLecture) {
+    return []oneLecture{
+       {Date: "Wednesday 22. September", Time: "10:00-13:00", Room: "A-343", Code: code},
+    }
+}
 
 // getLectures responds with the list of all lectures this week as JSON.
 func getLectures(c *gin.Context) {
+    code := c.Param("code")
+    lectures := getLecturesByCode(code)
     c.IndentedJSON(http.StatusOK, lectures)
 }
 
-func PageContent(link string)(string) {
+func pageContent(link string)(string) {
     res, err := http.Get(link)
     if err != nil {
         log.Fatal(err)
@@ -40,7 +44,7 @@ func PageContent(link string)(string) {
 
 func main() {
     router := gin.Default()
-    router.GET("/lectures", getLectures)
+    router.GET("/lectures/:code", getLectures)
 
     router.Run("localhost:8080")
 }
